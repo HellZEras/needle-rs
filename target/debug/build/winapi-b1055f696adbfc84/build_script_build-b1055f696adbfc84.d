d39de26027dd4dac8518f016c725bbd76b3f8986
2P1F <60047882+HellZEras@users.noreply.github.com>
2024-08-01 19:20:17 +0100

    Delete target directory

--- target/debug/build/winapi-b1055f696adbfc84/build_script_build-b1055f696adbfc84.d
+++
@@ -1,5 +0,0 @@
-D:\Projects\needle-rs\target\debug\build\winapi-b1055f696adbfc84\build_script_build-b1055f696adbfc84.exe: C:\Users\2P1F\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-D:\Projects\needle-rs\target\debug\build\winapi-b1055f696adbfc84\build_script_build-b1055f696adbfc84.d: C:\Users\2P1F\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs
-
-C:\Users\2P1F\.cargo\registry\src\index.crates.io-6f17d22bba15001f\winapi-0.3.9\build.rs:
